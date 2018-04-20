69f5a7977f73b6d9473f31104e60e7de1d5d9115
Boiar Qin <bq27@cornell.edu>
2018-04-20 16:34:33 -0500

    Add unit tests for testing grouping of roads with towns

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -31,7 +31,7 @@ describe ('Test grouping of unconnected road pieces into sets (no towns):', ()=>
     it ('returns 2 sets for 2 unconnected road pieces', () => {
         const myRoads = [
             {edge: [0,1], color: 'red'},
-            {edge: [3,2], color: 'red'},
+            {edge: [3,2], color: 'red'}
         ];
         const myTowns = [];
     
@@ -44,7 +44,7 @@ describe ('Test grouping of unconnected road pieces into sets (no towns):', ()=>
             {edge: [3,2], color: 'red'},
             {edge: [53,45], color: 'red'},
             {edge: [40,41], color: 'red'},
-            {edge: [11,21], color: 'red'},
+            {edge: [11,21], color: 'red'}
         ];
         const myTowns = [];
     
@@ -56,7 +56,7 @@ describe ('Test grouping of connected road pieces into sets (no towns):', ()=> {
     it ('returns 1 sets for 2 connected road pieces', () => {
         const myRoads = [
             {edge: [0,1], color: 'red'},
-            {edge: [1,2], color: 'red'},
+            {edge: [1,2], color: 'red'}
         ];
         const myTowns = [];
         
@@ -66,11 +66,77 @@ describe ('Test grouping of connected road pieces into sets (no towns):', ()=> {
     it ('returns 1 sets for 2 connected road pieces (different vtx order)', () => {
         const myRoads = [
             {edge: [0,1], color: 'red'},
-            {edge: [2,1], color: 'red'},
+            {edge: [2,1], color: 'red'}
         ];
         const myTowns = [];
         
         expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(1);
     });
 
+    it ('returns 1 sets for 3 connected road pieces', () => {
+        const myRoads = [
+            {edge: [0,1], color: 'red'},
+            {edge: [2,1], color: 'red'},
+            {edge: [0,8], color: 'red'}
+        ];
+        const myTowns = [];
+        
+        expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(1);
+    });
+
+    it ('returns 2 sets for 5 road pieces', () => {
+        const myRoads = [
+            {edge: [0,1], color: 'red'},
+            {edge: [2,1], color: 'red'},
+            {edge: [0,8], color: 'red'},
+            {edge: [34,44], color: 'red'},
+            {edge: [33,34], color: 'red'}
+        ];
+        const myTowns = [];
+        
+        expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(2);
+    });
+});
+
+describe ('Test breaking of connected road pieces by towns:', ()=> {
+    it ('returns 1 sets for 2 connected road pieces with intersecting town (same color)', () => {
+        const myRoads = [
+            {edge: [0,1], color: 'red'},
+            {edge: [1,2], color: 'red'}
+        ];
+        const myTowns = [
+            {vertex: 1, color: 'red'}
+        ];
+        
+        expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(1);
+    });
+
+    it ('returns 2 sets for 2 connected road pieces with intersecting town (diff color)', () => {
+        const myRoads = [
+            {edge: [0,1], color: 'red'},
+            {edge: [1,2], color: 'red'}
+        ];
+        const myTowns = [
+            {vertex: 1, color: 'green'}
+        ];
+        
+        expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(2);
+    });
+
+    it ('returns 3 sets for 5 road pieces', () => {
+        const myRoads = [
+            {edge: [0,1], color: 'red'},
+            {edge: [2,1], color: 'red'},
+            {edge: [0,8], color: 'red'},
+            {edge: [34,44], color: 'red'},
+            {edge: [33,34], color: 'red'}
+        ];
+        const myTowns = [
+            {vertex: 1, color: 'green'},
+            {vertex: 2, color: 'blue'},
+            {vertex: 34, color: 'red'}
+        ];
+        
+        expect(numUniqSets(divideIntoSets(myRoads, myTowns))).toEqual(3);
+    });
 });
